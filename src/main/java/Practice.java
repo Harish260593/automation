6e5e746fe2e54316e3a9c46a6ad02ef67ffb5d4a
Harish260593 <mharish455@gmail.com>
2023-11-05 09:39:53 +0530

    First commit

---
+++ src/main/java/Practice.java
@@ -0,0 +1,17 @@
+public class Practice
+{
+    public static void main (String [] args)
+    {
+        System.out.print("Hello World");
+        // Task
+        //Odd/Even program
+        // Operator Concept
+
+        // What is Maven
+        // Why maven is used
+        // What is build tool.
+        //Can we run selinium without maven - Yes
+        // Inheritance, Class, Object
+
+    }
+}
